b7dd96b27d1aa8a1865a7db1a3674590823c3560
colewi95 <cole.williamson95@gmail.com>
2019-12-10 12:23:05 -0500

    Update App.js
    2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Take Me To Your Master
         </a>
       </header>
     </div>
